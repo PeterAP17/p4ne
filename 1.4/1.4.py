f19f3e4fda09b3fe05e0526599da59cd5b9721df
PeterAP17 <ppochinko@gmail.com>
2025-06-23 23:00:29 +0300

    my 5 python program

--- 1.4/1.4.py
+++ 1.4/1.4.py
@@ -4,18 +4,18 @@ listOfIPv4 = []
 
 class IPv4RandomNetwork (ipaddress.IPv4Network):
    def __init__(self):
-       network = random.randint(0x0B000000, 0xDF000000)
-       prefix = random.randint(8, 24)
-       ipaddress.IPv4Network.__init__(self,(network, prefix),strict=False)
+       self.network = random.randint(0x0B000000, 0xDF000000)
+       self.prefix = random.randint(8, 24)
+       ipaddress.IPv4Network.__init__(self,(self.network, self.prefix),strict=False)
 
    def is_regular(self):
        return self.is_global
 
-   def key_value(self):
-       return int(self.network_address) + (int(self.netmask) << 32)
+   def networkPrefixToNumber(self):
+       return (int(self.network) + (int(self.prefix) << 32))
 
-def sortfunc(x):
-    return x.key_value()
+def simplefunc(x):
+    return x.networkPrefixToNumber()
 
 i = 0
 while i<100:
@@ -23,6 +23,9 @@ while i<100:
     listOfIPv4.append (random_network)
     i = i + 1
 
-for n in sorted(listOfIPv4, key=sortfunc):
+#print (sorted(listOfIPv4, key=simplefunc))
+
+for n in sorted(listOfIPv4, key=simplefunc):
     print(n)
+
 
