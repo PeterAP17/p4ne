783bafaa6539cbd214ea59048254c5a7af495ea7
PeterAP17 <ppochinko@gmail.com>
2025-06-27 00:04:01 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	1.4/1.4.py

--- 1.5/1.5.py
+++ 1.5/1.5.py
@@ -1,27 +1,30 @@
 import glob
 ListOfIP=[]
-count = 0
+#count = 0
 
 files=glob.iglob('C://p4ne//log_files//*.log')
+#print(type(files))
 
 for i in files:
+    print(i)
     with open (i) as file:
         for j in file:
-            count = count + 1
+            #count = count + 1
             raw_line = j
             line = raw_line.strip()
 
             if line.find('ip address')==0:
                 lineWithoutText = line.replace("ip address", "")
                 PureLine = lineWithoutText.strip()
-                print (PureLine)
+                #print (PureLine)
                 ListOfIP.append(PureLine)
+    #i.close() - так и не понял, почему эта штука не работает (((
 
 SetOfIP = set(ListOfIP)
 ListOfIP = list (SetOfIP)
 
-for i in ListOfIP:
-    print (i)
+for count in ListOfIP:
+    print ("|%-40s|" % count)#,"|%20s|" % i.network_address, "|%20s|" % i.netmask)
 
 
 
